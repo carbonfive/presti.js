9850abc17ba3eee3ddb8a01e2c640d6f1d2bbfa7
Rudy Jahchan <dev+rudyjahchan@carbonfive.com>
2012-04-22 21:09:25 -0700

    Authenticate Users via Google Oauth2

--- server.js
+++ server.js
@@ -1,13 +1,20 @@
 var express = require('express');
 var app = express.createServer();
 var io = require('socket.io').listen(app);
+var auth = require('connect-auth');
 var show = require('./lib/show').init(io);
 var slides = require('./lib/slides').parse('slides.md');
-
-console.log(slides);
+var config = require('./config');
 
 app.configure(function() {
   app.use(express.errorHandler({ showStack: true, dumpExceptions: true }));
+  app.use(express.cookieParser());
+  app.use(express.session({ secret: config.sessionSecret }));
+  app.use(auth([ auth.Google2(
+    { appId: config.googleAppId,
+      appSecret: config.googleAppSecret,
+      callback: config.googleCallback,
+      requestEmailPermission: true }) ]));
   app.set('view engine','jade');
   app.set('view options', { layout: false });
   app.use(express.bodyParser());
@@ -19,8 +26,47 @@ app.get('/', function(request, response, next) {
   response.render('home', { slides : slides });
 });
 
-app.get('/chat', function(request, response, next) {
+app.get('/chat', authenticate, function(request, response, next) {
   response.render('chat', { slides : slides });
 });
 
+app.get('/logout', function(request, response, next) {
+  if(request.isAuthenticated()) {
+    request.logout();
+  }
+  response.redirect('/');
+});
+
+app.get('/present', authenticate, isPresenter, function(request, response, next) {
+  response.render('present', { slides : slides });
+});
+
 app.listen(8500);
+
+function authenticate(request, response, next) {
+  if(request.isAuthenticated()) next();
+  else {
+    request.authenticate(function(error, authenticated) {
+      if(error) next(new Error('Problem authenticating'));
+      else if(authenticated === true) {
+        console.log(requested.getAuthDetails());
+        next();
+      }
+      else if(authenticated === false) {
+        next(new Error('Access Denied!'));
+      } else {}
+    });
+  }
+}
+
+function isPresenter(request, response, next) {
+  if(request.isAuthenticated()) {
+    var userAuthDetails = request.getAuthDetails();
+    console.log(userAuthDetails);
+    if(userAuthDetails.user.email == 'rudy@carbonfive.com' ||
+       userAuthDetails.user.email == 'alex@carbonfive.com') {
+      return next();
+    }
+  }
+  response.redirect('/');
+}
